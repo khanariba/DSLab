0aa636f2d53f5121f633ce4923787684b826adb9
aiktc <shaikhhgr@gmail.com>
2017-07-12 12:32:30 +0530

    First commit: stack program

---
+++ stack.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_SIZE 10
+
+int stack (MAX_SIZE),TOP=-1;
+
+int isFULL()
+{
+	//returns 1 if stack is full else return -1
+	return (MAX_SIZE)=1? return -1
+}
+
+int isEMPTY()
+{
+	//return 1 if stack is empty else returns -1
+	if stack==EMPTY;
+	return 1;
+	else return -1;
+}
+
+int peek()
+{
+return element at the top of stack
+return element at the top
+}
+
+void push(int e) 
+{
+	//insert an element into stack
+	if (stack=!MAX_SIZE)
+	{
+		(top element==e;e++)
+	}
+	else
+	{
+		stack overflow
+	}	
+		
+}
+
+void pop()
+{
+	//deletes an element into the block
+	int d;
+	if(stack is not empty)
+	
+}
