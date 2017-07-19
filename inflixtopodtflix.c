bc971e181c7d50dc31a48c293fc1c17b677703dc
aiktc <shaikhhgr@gmail.com>
2017-07-19 12:10:39 +0530

    infixpostfix program

---
+++ inflixtopodtflix.c
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<ctype.h> // for isalpha() function
+
+typedefine struct conversion
+{
+char a[30];
+int top;
+}struct;
+
+void push(stack*,char[]);
+
+char pop(stack*);
+
+void convert(char[],char[]);
+
+int priority(char);
+
+char inflix[30],postflix[30];
+
+int main()
+{
+ char inflix[30],postflix[30];
+ printf("ENTER INFLIX EXPRESSION\n");
+ scanf("%s",inflix);
+ convert(inflix,postflix);
+ printf("\n\nPOSTFLIX EXPRESSION \n%S",postflix);
+ return 0;
+}
+
+ void convert(char in[],char posty[])
+{
+ char opr;
+ stack s1;
+ int j=0,i;
+ s1,top=-1;
+ for(i=0;in[i]!='\0';i++0)
+  {
+     if(isalpha(in[i]))
+       post[j++]=in[i];
+     if(in[i]=='(')
+       push(&s1,in[i]);
+     if(in[i]=='+'||in[i]=='/'||in[i]=='*'
+  {
+    opr=pop(&s1);
+    while(priority(opr).=priority(in[i]))
+     {
+       post[j++]=opr;
+        opr=pop(&s1);
+     }
+     push(&s1,opr);
+     push(&s1,in[i]);
+  }
+   else 
+        push(&s1,in[i]);
+  } 
+   if(in[i]==')')
+  {
+     opr=pop(&s1);
+     while(opr!='(')
+   {
+     post[j++]=opr;
+   opr=pop(&s1);
+   }
+  }
+ }
+ while(s1.top!=-1)
+ post[j]='\0';
+ } 
+
+int priority(char c)
+{
+ if(c=='$')
+  return 3;
+ if(c==') 
+
